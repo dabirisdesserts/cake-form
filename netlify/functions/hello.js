a55ab7ce9d9be88db1f8d2a1762370a38edb8213
Jordon H Griffin <dabirisdesserts@gmail.com>
2025-10-25 19:26:12 -0400

    Try ES modules format for Netlify

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,4 +1,4 @@
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
     return {
         statusCode: 200,
         body: JSON.stringify({ message: 'Hello from Netlify Function!' })
