5b049e56c5aa2f1684dcd6b9499e9fad0329df98
Jordon H Griffin <dabirisdesserts@gmail.com>
2025-10-25 19:23:26 -0400

    Add Netlify function for form submission

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require('path');
 module.exports = (req, res) => {
   // Set content type to HTML
   res.setHeader('Content-Type', 'text/html');
-  
+
   // Read and serve the index.html file
   const htmlPath = path.join(__dirname, 'index.html');
-  
+
   try {
     const htmlContent = fs.readFileSync(htmlPath, 'utf8');
     res.status(200).send(htmlContent);
