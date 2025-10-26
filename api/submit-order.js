b2256d45c085f4b75ab5b281f06e78c094dd2f8b
Jordon H Griffin <dabirisdesserts@gmail.com>
2025-10-26 18:12:35 -0400

    Fix nodemailer typo causing 500 error
    - Change createTransporter to createTransport
    - Fix server error preventing form submission
    - Resolve 'A server e' JSON parsing error

--- api/submit-order.js
+++ api/submit-order.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 const https = require('https');
 
 // Email configuration using environment variables
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.gmail.com',
     port: process.env.EMAIL_PORT || 587,
     secure: false,
